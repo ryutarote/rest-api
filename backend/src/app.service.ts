31ac70df9f790dc216bb97b309d8352a5393eb75
ryutarote <75284855+ryutarote@users.noreply.github.com>
2025-01-01 00:34:10 +0900

    Modify service

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -2,7 +2,17 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getModuleInfo() {
+    return {
+      imports: [
+        'ConfigModule (isGlobal: true)',
+        'AuthModule',
+        'UserModule',
+        'TodoModule',
+        'PrismaModule',
+      ],
+      controllers: ['AppController'],
+      providers: ['AppService'],
+    };
   }
 }
